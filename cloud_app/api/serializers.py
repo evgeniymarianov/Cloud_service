c04ebf84b4653f896d3c003376a44d595716a53a
evgeniymarianov <evgeniymarianov@gmail.com>
2020-08-19 01:33:35 +0300

    Сделал дампы, в API добавил вывод количества networks для каждой ВМ

--- cloud_app/api/serializers.py
+++ cloud_app/api/serializers.py
@@ -5,10 +5,18 @@ from core.models import VirtualMachine
 
 class VirtualMachineListSerializer(serializers.ModelSerializer):
     networks = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    networks_count = serializers.SerializerMethodField()
+    some = serializers.SerializerMethodField()
+
     class Meta:
         model = VirtualMachine
-        fields = ("current_user", "ram", "cpu", "hdd_type", "hdd_capacity", "networks")
+        fields = ("current_user", "ram", "cpu", "hdd_type", "hdd_capacity", "networks", "networks_count", "some")
 
+    def get_networks_count(self, obj):
+        return obj.networks.count()
+
+    def get_some(self, obj):
+        return 'hi'
 
 class VirtualMachineDetailSerializer(serializers.ModelSerializer):
     networks = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
